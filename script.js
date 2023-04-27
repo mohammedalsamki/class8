d4277cd297add27f095070152b54bf3a00863bdb
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-04-27 16:58:22 +0300

    added js file

---
+++ script.js
@@ -0,0 +1,77 @@
+function myFunction() {
+    let x = 50;
+    let y = 6;
+
+    document.getElementById("demo").innerHTML = x + y;
+}
+var a = 10;
+// how to declare a variable in javascript
+// var, let, const
+// what is var??
+
+// var is a keyword which is used to declare a variable in javascript
+
+
+var c = ' Hello World';
+console.log(a);
+// console.log('hellow i am js from contact page')
+
+// what is let ??
+
+// let is a keyword which is used to declare a variable in javascript
+
+
+let b = 20;
+
+console.log(b);
+
+// what is const ??
+
+// const is a keyword which is used to declare a variable in javascript
+
+const d = 30;
+console.log(d);
+
+// what is the difference between var, let and const ??
+
+// var and let both are same but the difference is var is a global variable and let is a local variable
+
+// const is a variable which value is not changeable
+
+// what is the data type in js ??
+
+// string, number, boolean, object, undefined, null
+
+
+// what is the string ??
+
+// string is a data type which is used to store a string value
+
+var test = 'hellow world';
+console.log(test);
+
+// what is the number ??
+
+// number is a data type which is used to store a number value
+
+var test1 = 10;
+console.log(test1);
+
+// what is the boolean ??
+
+// boolean is a data type which is used to store a true or false value
+
+var test2 = false;
+console.log(test2);
+
+// what is the object ??
+
+// object is a data type which is used to store a object value
+
+var test3 = {
+    // key: value
+    name: 'hellow',
+    age: 20,
+    address: 'dhaka'
+}
+console.log(test3);
